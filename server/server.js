d674edf524b350bb1e17b0bd908dd8ffae06a303
dee4569 <dilshanicosta0506@gmail.com>
2019-01-24 20:30:55 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -10,25 +10,23 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    // var todo = new Todo({
-    //     text: req.body.text
-    // });
-
-    // todo.save().then((doc) => {
-    //     res.send(doc);
-    // }, (e) => {
-    //     res.status(400).send(e);
-    // });
-
-    var todo2 = new Todo({
+    var todo = new Todo({
         text: req.body.text
     });
 
-    todo2.save().then((doc) => {
+    todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
-    })
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.listen(3000, () => {
