ccb26e3b9d746102598240729cb2c8c16230b5b0
dee4569 <dilshanicosta0506@gmail.com>
2019-01-28 14:49:19 +0530

    Changed server file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MongoDB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MongoDB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
