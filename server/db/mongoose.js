1111a972e265b18ecf92e300164293121f5cf780
dee4569 <dilshanicosta0506@gmail.com>
2019-01-28 19:23:35 +0530

    Adjusted server.js file when database was set up with heroku config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,6 @@
 var mongoose = require('mongoose');
-require('dotenv').config();
-
-console.log(process.env.MongoDB_URI);
-console.log(typeof(process.env.MongoDB_URI));
 
 mongoose.Promise = global.Promise;
-mongoose.connect((process.env.MongoDB_URI || 'mongodb://localhost:27017/TodoApp'), { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
