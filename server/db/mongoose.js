cff482c4675c0849b5952bf79d568f6f04b113c1
dee4569 <dilshanicosta0506@gmail.com>
2019-01-28 20:04:16 +0530

    changed mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,29 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'), { useNewUrlParser: true });
+// mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'), { useNewUrlParser: true });
+
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: process.env.MONGODB_URI
+};
+ 
+mongoose.connect(db.localhost,
+    {
+        useMongoClient: true
+    }
+).then(
+    () => {},
+    err => 
+    {
+        mongoose.connect(db.mlab,
+            {
+                useMongoClient: true
+            }
+        );
+    }
+);
+ 
 
 module.exports = {mongoose};
 
