5acde7ada6cd6d05880afe141b541335e5da816f
dee4569 <dilshanicosta0506@gmail.com>
2019-01-28 16:13:50 +0530

    updated mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 require('dotenv').config();
 
+console.log(process.env.MongoDB_URI);
+console.log(typeof(process.env.MongoDB_URI));
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MongoDB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect((process.env.MongoDB_URI || 'mongodb://localhost:27017/TodoApp'), { useNewUrlParser: true });
 
 module.exports = {mongoose};
