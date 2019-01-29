ef22c7eaf81b3b806c2c3f161f983936996dc95e
dee4569 <dilshanicosta0506@gmail.com>
2019-01-29 08:33:35 +0530

    Changed the mongoose.js file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,28 +1,28 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'), { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'), { useNewUrlParser: true });
 
-var db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: process.env.MONGODB_URI
-};
+// var db = {
+//     localhost: 'mongodb://localhost:27017/TodoApp',
+//     mlab: process.env.MONGODB_URI
+// };
  
-mongoose.connect(db.localhost,
-    {
-        useNewUrlParser: true
-    }
-).then(
-    () => {},
-    err => 
-    {
-        mongoose.connect(db.mlab,
-            {
-                useNewUrlParser: true
-            }
-        );
-    }
-);
+// mongoose.connect(db.localhost,
+//     {
+//         useNewUrlParser: true
+//     }
+// ).then(
+//     () => {},
+//     err => 
+//     {
+//         mongoose.connect(db.mlab,
+//             {
+//                 useNewUrlParser: true
+//             }
+//         );
+//     }
+// );
  
 
 module.exports = {mongoose};
