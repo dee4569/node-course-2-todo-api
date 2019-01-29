b1fe9fb0b3124c4b16735a5160f1af31598126a6
dee4569 <dilshanicosta0506@gmail.com>
2019-01-29 21:11:15 +0530

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 //will store the properties for the user
 var UserSchema = new mongoose.Schema({
@@ -90,6 +91,23 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save', function (next){
+    var user = this;
+
+    //returns true if password is modified
+    //we need the encryption to run if the password is modified
+    if(user.isModified('password')){
+        bcrypt.genSalt(10, (err,salt) =>{
+            bcrypt.hash(user.password,salt, (err, hash) =>{
+                user.password = hash;
+                next();
+            });
+        })
+    }else{
+        next();
+    }
+});
 
 //defining the user model with the required properties
 var User = mongoose.model('Users',UserSchema);
