49e3f9832491805bfcbc382bcd9731bf11b59880
dee4569 <dilshanicosta0506@gmail.com>
2019-02-02 12:10:09 +0530

    Update config to user JSON file

--- server/models/user.js
+++ server/models/user.js
@@ -60,7 +60,7 @@ UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
     //this returns an object; because of that to string method is used
-    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString(); 
+    var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString(); 
     
     user.tokens = user.tokens.concat([{access, token}]);
 
@@ -86,7 +86,7 @@ UserSchema.statics.findByToken = function (token) {
     var decoded;
     
     try{
-        decoded = jwt.verify(token, 'abc123')
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     }catch (e){
         // return new Promise((resolve, reject) => {
         //     reject();
